36b78c65a0cfb9f8d42312c533d2aefa443a85ea
DuyDDinh <86141769+DuyDDinh@users.noreply.github.com>
2021-09-15 13:39:23 +0700

    bai1_javabasic

---
+++ newjava/src/Bai1.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Scanner;
+public class Bai1 {
+    public static void main(String[] args) throws Exception {
+
+         Scanner sc = new Scanner(System.in);
+         String message ; 
+         System.out.println(" Nhap vao chuoi ky tu ");
+         message = sc.nextLine();
+        char[] charArray = message.toCharArray();
+        for( int i = 0 ; i < charArray.length ;i++){
+            if( charArray[i] >= 97 && charArray[i]<= 122){
+                charArray[i] -=32 ;
+
+            }
+        }
+        // char[] charArray = message.toCharArray();
+    //sử dụng vòng lặp for để duyệt các phần tử trong charArray
+    // for(int i = 0; i < charArray.length; i++) {
+    //   if(charArray[i] >= 97 && charArray[i] <= 122){
+    //     charArray[i] -= 32;
+    //   }
+    // }
+     
+        message=String.valueOf(charArray);
+        System.out.println("chuoi sau khi doi "+ message);
+            
+     
+    }
+}
