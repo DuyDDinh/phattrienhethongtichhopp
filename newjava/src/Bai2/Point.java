dcb353da0fd3e5609cc5929f780fbd10aa74be12
DuyDDinh <86141769+DuyDDinh@users.noreply.github.com>
2021-09-17 19:16:57 +0700

    Create Point.java

---
+++ newjava/src/Bai2/Point.java
@@ -0,0 +1,25 @@
+package Bai2;
+
+import java.util.Scanner;
+
+public class Point {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println(" Nhap toa do ");
+            int x1  = scanner.nextInt();
+            int y1  = scanner.nextInt();
+        System.out.println(" Toa do diem vua nhap la : ("+x1+","+y1+")");
+        System.out.println("khoang cach la "+ khoangCach(x1, y1)); 
+        
+
+    }
+    public static double khoangCach(int x1, int y1) {
+        double khoancach = Math.sqrt(x1*x1 + y1*y1);
+
+        return khoancach ;
+
+       
+    }
+
+    
+}
